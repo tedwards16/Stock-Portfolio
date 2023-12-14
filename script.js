16f7f5637cb547725522ff2da69af81355499cdf
tedwards16 <139355748+tedwards16@users.noreply.github.com>
2023-12-14 17:55:41 +0000

    Editable Inputs

--- script.js
+++ script.js
@@ -22,10 +22,18 @@ class Portfolio extends React.Component {
       ]
     };
     this.removeStock = this.removeStock.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
     removeStock(index) {
       const portfolio = this.state.portfolio.slice(); // shallow copy
       portfolio.splice(index, 1); // remove value at index
+      this.setState({ portfolio });
+    }
+
+    handleChange(event, index) {
+      const portfolio = this.state.portfolio.slice(); // shallow copy
+      const { name, value } = event.target;
+      portfolio[index][name] = value;
       this.setState({ portfolio });
     }
   
@@ -69,9 +77,9 @@ class Portfolio extends React.Component {
                   return (
                     <tr key={index}>
                       <td>{name}</td>
-                      <td><input type="number" name="shares_owned" value={shares_owned} /></td>
-                      <td><input type="number" name="cost_per_share" value={cost_per_share} /></td>
-                      <td><input type="number" name="market_price" value={market_price} /></td>
+                      <td><input onChange={e => this.handleChange(e, index)} type="number" name="shares_owned" value={shares_owned} /></td>
+                      <td><input onChange={e => this.handleChange(e, index)} type="number" name="cost_per_share" value={cost_per_share} /></td>
+                      <td><input onChange={e => this.handleChange(e, index)} type="number" name="market_price" value={market_price} /></td>
                       <td>{market_value}</td>
                       <td>{unrealized_gain_loss}</td>
                       <td><button className="btn btn-light btn-sm"  onClick={() => this.removeStock(index)}>remove</button></td>
